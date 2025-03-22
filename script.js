166eb2f5d5927598148e05ad544b22b44a2079e2
AryanRajdev <91838247+AryanRajdev@users.noreply.github.com>
2025-03-22 22:21:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 function getFormvalue() {
     //Write your code here
+	let fname = document.querySelector('input[name="fname"]').value;
+	let lname = document.querySelector('input[name="lname"]').value;
 
+	fname = fname.trim();
+	lname = lname.trim();
+
+	if(fname.length === 0 || lname.length ===0){
+		alert('Please specify fname and lname')
+	}
+	else
+	alert(`${fname} ${lname}`);
+	
 }
